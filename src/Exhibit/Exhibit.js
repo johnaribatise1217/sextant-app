f3ffc027f28855298e1e1c67722639445c810587
John Aribatise <johnaribatise@hsass.org>
2023-07-30 07:53:19 +0100

    update repository

---
+++ src/Exhibit/Exhibit.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './exhibit.css'
+
+const Exhibit = ({title}) => {
+  return (
+    <div className='exhibit'>
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+export default Exhibit
